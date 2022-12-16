ecc27bc1dadb35d6919f1d7fa8b94bee671fc7e8
darkelle <djilani.cardineau@epitech.eu>
2022-12-16 16:12:11 +0100

    push parser

---
+++ parser.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func getPath() (string, error) {
+	arg_len := len(os.Args[0:])
+	if arg_len != 2 {
+		fmt.Println("wrong number of argument")
+		return "", errors.New("wrong number of argument")
+	}
+
+	return os.Args[1], nil
+}
+
+func Parser() error {
+
+	path, err := getPath()
+
+	if err != nil {
+		return err
+	}
+
+	data, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	// Conversion des bytes en chaîne de caractères
+	str := string(data)
+
+	fmt.Println(str)
+
+	return nil
+}
