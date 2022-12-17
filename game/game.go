988c8ff42220e7ea87af78c69350fded7a17a459
baraou <corentin.baraou@gmail.com>
2022-12-17 16:56:08 +0100

    added error handling and 'all' methods

--- game/game.go
+++ game/game.go
@@ -1,6 +1,9 @@
 package game
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type TypeTool int
 
@@ -22,7 +25,7 @@ type Weight interface {
 }
 
 type Movable interface {
-	Move(int, int, [][]Floor)
+	Move(int, int, *[][]Floor) error
 	Get_status() string
 }
 
@@ -47,7 +50,7 @@ type Camion struct {
 	status       string
 	x            int
 	y            int
-	colis_list   *Colis
+	colis_list   []*Colis
 } //(tool, weight, movable, waiter)
 
 type Transpalette struct {
@@ -66,12 +69,12 @@ type Colis struct {
 } //(tool, weight)
 
 type game_functions interface {
-	Set_turns(int)                            // x y
-	Get_turns(int) int                        // x y
-	Create_map(int, int)                      // x y
-	Create_transpallete(string, int, int)     // name, x, y
-	Create_colis(string, int, int, int)       // name, x, y, weight
-	Create_camion(string, int, int, int, int) // name, x, y, max_weight, turn_max
+	Set_turns(int)                                  // x y
+	Get_turns() int                                 // x y
+	Create_map(int, int) error                      // x y
+	Create_transpallete(string, int, int) error     // name, x, y
+	Create_colis(string, int, int, int) error       // name, x, y, weight
+	Create_camion(string, int, int, int, int) error // name, x, y, max_weight, turn_max
 	Next_turn()
 }
 
@@ -88,51 +91,97 @@ func (game *Game) Set_turns(turns int) {
 	game.turns = turns
 }
 
-func (game *Game) Get_turns(turns int) int {
+func (game *Game) Get_turns() int {
 	return game.turns
 }
 
-func (game *Game) Create_map(x, y int) {
+func (game *Game) Create_map(x, y int) error {
+	if game.Map != nil {
+		return errors.New("map is already created")
+	}
+	if x <= 0 || y <= 0 {
+		return errors.New("map can't be of negative length")
+	}
 	game.Map = make([][]Floor, x)
 	for i := range game.Map {
 		game.Map[i] = make([]Floor, y)
 	}
+	return nil
 }
 
-func (game *Game) Create_transpallete(name string, x, y int) {
-	var trans Tool
-	trans = &Transpalette{name: name, x: x, y: y}
+func checkToolCreation(game *Game, x, y int) error {
+	xlen := len(game.Map)
+	ylen := len(game.Map[0])
+	if game.Map[x][y].Tool != nil {
+		return errors.New("can't create here this cell is already occupied")
+	}
+	if (x < 0 && x >= xlen) || (y < 0 && y >= ylen) {
+		return errors.New("tool must be created within the map")
+	}
+	return nil
+}
+
+func (game *Game) Create_transpallete(name string, x, y int) error {
+	if err := checkToolCreation(game, x, y); err != nil {
+		return err
+	}
+	var trans Tool = &Transpalette{name: name, x: x, y: y}
 	game.ToolsList = append(game.ToolsList, trans)
 	tile := Floor{Tool: trans}
 	game.Map[x][y] = tile
+	return nil
 }
 
-func (game *Game) Create_colis(name string, x, y, weight int) {
-	var pack Tool
-	pack = &Colis{name: name, x: x, y: y, size: weight}
+func (game *Game) Create_colis(name string, x, y, weight int) error {
+	if err := checkToolCreation(game, x, y); err != nil {
+		return err
+	}
+	if weight <= 0 {
+		return errors.New("colis can't be created with a negative weight")
+	}
+	var pack Tool = &Colis{name: name, x: x, y: y, size: weight}
 	game.ToolsList = append(game.ToolsList, pack)
 	tile := Floor{Tool: pack}
 	game.Map[x][y] = tile
+	return nil
 }
 
-func (game *Game) Create_camion(name string, x, y, max_weight, turn_max int) {
-	var truck Tool
-	truck = &Camion{name: name, x: x, y: y, poids_max: max_weight, turn_max: turn_max}
+func (game *Game) Create_camion(name string, x, y, max_weight, turn_max int) error {
+	if err := checkToolCreation(game, x, y); err != nil {
+		return err
+	}
+	if max_weight <= 0 {
+		return errors.New("camion can't be created with a negative max weight")
+	}
+	if turn_max <= 0 {
+		return errors.New("camion can't be created with a negative turn max")
+	}
+	var truck Tool = &Camion{name: name, x: x, y: y, poids_max: max_weight, turn_max: turn_max}
 	game.ToolsList = append(game.ToolsList, truck)
 	tile := Floor{Tool: truck}
 	game.Map[x][y] = tile
+	return nil
 }
 
 func (game *Game) Next_turn() {
 	// apelle next_turn sur tout les waiter?
 	game.Turn += 1
-	fmt.Println("Tour ", game.Turn)
+	fmt.Println("Tour", game.Turn)
+	// for _, v := range game.Map {
+	// 	fmt.Println(v)
+	// }
+	// fmt.Println(game.ToolsList)
+	// time.Sleep(time.Second)
 }
 
 // Camion methods
-func (truck *Camion) Move(int, int, [][]Floor) {
+func (truck *Camion) Move(int, int, *[][]Floor) error {
+	if !truck.Is_present() {
+		return errors.New("truck can't move because it is GONE")
+	}
 	truck.turn_current = truck.turn_max
 	truck.status = "GONE"
+	return nil
 }
 
 func (truck *Camion) Wait() {
@@ -154,6 +203,9 @@ func (truck *Camion) Is_present() bool {
 func (truck *Camion) Next_turn() {
 	if truck.turn_current > 0 {
 		truck.turn_current -= 1
+		if truck.turn_current == 0 {
+			truck.colis_list = []*Colis{}
+		}
 	}
 }
 
@@ -182,14 +234,63 @@ func (truck *Camion) Get_position() (int, int) {
 	return truck.x, truck.y
 }
 
+func (truck *Camion) AddPackage(pack *Colis) {
+	// error if > max weight
+	truck.colis_list = append(truck.colis_list, pack)
+}
+
 // Transpalette methods
 
-func (trans *Transpalette) Move(int, int, [][]Floor) {
-	// update trans.X trans.Y to X Y and move floor tile at trans.X trans.Y to X Y
+func (trans *Transpalette) Move(x, y int, floor *[][]Floor) error {
+	xlen := len(*floor)
+	ylen := len((*floor)[0])
+	newx := trans.x + x
+	newy := trans.y + y
+	if (x != 0 && y != 0) || x > 1 || x < -1 || y > 1 || y < -1 {
+		return errors.New("can't move more than one cell")
+	}
+	if (*floor)[newx][newy].Tool != nil {
+		return errors.New("can't move on an occupied cell")
+	}
+	if (newx < 0 && newx >= xlen) || (newy < 0 && newy >= ylen) {
+		return errors.New("cannot move out of the map")
+	}
+	if (*floor)[newx][trans.y+y] == (Floor{}) {
+		(*floor)[newx][trans.y+y].Tool = trans
+		(*floor)[trans.x][trans.y].Tool = nil
+		trans.x = newx
+		trans.y = newy
+	}
+	trans.status = "GO"
+	return nil
 }
 
 func (trans *Transpalette) Wait() {
 	trans.status = "WAIT"
+}
+
+func (trans *Transpalette) Take(x, y int, floor *[][]Floor) error {
+	var tile = (*floor)[x][y]
+	pack, ok := tile.Tool.(*Colis)
+	if !ok || trans.colis != nil {
+		return errors.New("can't pickup package on this tile")
+	}
+	trans.colis = pack
+	(*floor)[x][y].Tool = nil
+	trans.status = "TAKE"
+	return nil
+}
+
+func (trans *Transpalette) Drop(x, y int, floor *[][]Floor) error {
+	var tile = (*floor)[x][y]
+	truck, ok := tile.Tool.(*Camion)
+	if !ok || trans.colis == nil || !truck.Is_present() {
+		return errors.New("can't drop package on this tile")
+	}
+	truck.AddPackage(trans.colis)
+	trans.colis = nil
+	trans.status = "LEAVE"
+	return nil
 }
 
 func (trans *Transpalette) Get_name() string {
--- main.go
+++ main.go
@@ -6,11 +6,36 @@ import (
 	"github.com/darkelle24/go-epitech/game"
 )
 
+func handlePanics() {
+	if err := recover(); err != nil {
+		fmt.Println("😱", err)
+	}
+}
+
 func main() {
+	defer handlePanics()
 	var gameEnv game.Game
+	fmt.Println(gameEnv)
 	gameEnv.Create_map(10, 10)
 	gameEnv.Create_camion("test_camion", 1, 1, 1000, 5)
-	gameEnv.Create_camion("test_camion_2", 1, 5, 1000, 5)
-	gameEnv.Create_camion("test_camion_3", 5, 1, 1000, 5)
-	fmt.Println(gameEnv, gameEnv.Map[1][1].Tool.Get_name())
+	gameEnv.Create_colis("test_colis", 5, 2, 200)
+	gameEnv.Create_transpallete("test_transpallete", 5, 1)
+	// fmt.Println(gameEnv, gameEnv.Map[1][1].Tool.Get_name(), gameEnv.Map[1][5].Tool.Get_name(), gameEnv.Map[5][1].Tool.Get_name())
+	s, ok := gameEnv.Map[5][1].Tool.(*game.Transpalette)
+	truck := gameEnv.Map[1][1].Tool.(*game.Camion)
+	fmt.Println(s, ok, s.Get_name())
+	var floor = &gameEnv.Map
+	fmt.Println((*floor)[5][1].Tool)
+	gameEnv.Next_turn()
+	s.Take(5, 2, floor)
+	s.Move(-1, 0, floor)
+	s.Move(-1, 0, floor)
+	s.Move(-1, 0, floor)
+	fmt.Println(s, truck)
+	s.Drop(1, 1, floor)
+	fmt.Println(s, truck)
+	gameEnv.Next_turn()
+	err := s.Move(-1, 0, floor)
+	fmt.Println(err)
+	gameEnv.Next_turn()
 }
