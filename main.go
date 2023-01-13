d05118b3063f9da8a755933e594236e7f16883c5
Laroustine <laroustine@gmail.com>
2023-01-13 21:15:35 +0100

    [UPD] Main is final

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// Execute the program
 package main
 
 import (
@@ -21,30 +22,12 @@ func main() {
 	if parser.Parser(&gameEnv) != nil {
 		return
 	}
+	orders := algo.SetupManager(&gameEnv)
 
 	for !gameEnv.IsDone() {
-		for _, trans := range gameEnv.Transps {
-			trans.Move(0, 0, &gameEnv.Map)
-		}
-		for _, truck := range gameEnv.Trucks {
-			algo.SendTruck(truck, &gameEnv)
-		}
-		game.UpdateMap(&gameEnv)
+		orders = algo.UpdateManager(&gameEnv, orders)
+		_ = algo.UpdateTrucks(&gameEnv)
 		gameEnv.NextTurn()
 	}
-
-	// JUSTIN REMOVE
-	// x, y := gameEnv.Transps[0].Get_position()
-	// gameEnv.Transps[0].Take(x-1, y, &gameEnv.Map)
-	// gameEnv.Trucks[0].Wait()
-	// gameEnv.NextTurn()
-	// gameEnv.Transps[0].Move(1, 0, &gameEnv.Map)
-	// gameEnv.Trucks[0].Wait()
-	// gameEnv.NextTurn()
-	// gameEnv.Transps[0].Drop(x+2, y, &gameEnv.Map)
-	// gameEnv.Trucks[0].Move(0, 0, &gameEnv.Map)
-	// gameEnv.NextTurn()
-
 	fmt.Println(gameEnv.EndStateCharacter())
-
 }
